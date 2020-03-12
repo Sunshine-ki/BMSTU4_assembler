be2c14516664f908ffaae55e7427353b93cfe240
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-03-12 14:26:02 +0300

    Добавлены комментарии

--- lab_03/TASK/lab_03_2.asm
+++ lab_03/TASK/lab_03_2.asm
@@ -1,23 +1,34 @@
+;Описываем идентификатор, 
+;как доступный из других модулей.
 PUBLIC print_a
 
+; Объявляем сегмент, в котором будет символ "A"
 DATA_PRINT SEGMENT para public 'DATA'
-    sumb    DB "A"
-            DB '$'
+    sumb    db "A"
+            db '$'
 DATA_PRINT ENDS
 
+; Сегмент кода.
 SC2 SEGMENT para public 'CODE'
-    assume  DS:DATA_PRINT
+    ; Контролируем правильное обращение к переменным.
+    assume  CS:SC2, DS:DATA_PRINT
 print_a:
-    mov ax, DATA_PRINT
-    mov ds, ax
+    ; Кладем в DS данные для печати.
+    mov AX, DATA_PRINT
+    mov DS, ax
 
+    ; 09h - Записать строку в STDOUT.
+    ; DS:DX - адрес строки, заканчивающийся символом "$".
     MOV DX, offset sumb
     MOV AH, 09h
+
+    ; Пока что CX != 0 выводить "A".
 print:
     int 21h
     LOOP print
 
-    mov ax, 4c00h
+    ; Завершение программы.
+    mov AX, 4c00h
 	int 21h
 SC2 ENDS
 END
